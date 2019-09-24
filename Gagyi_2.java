540c27cacbd78a7091b0e2b6bd7e73693395443c
kzoltan99 <kzotya99@gmail.com>
2019-09-24 13:20:54 +0200

    Create Gagyi_2.java

---
+++ Gagyi_2.java
@@ -0,0 +1,12 @@
+public class Gagyi_2 {
+
+	public static void main(String[] args) {
+	
+		Integer x = 128;
+		Integer t = 128;
+
+		while (x <= t && x >= t && t!=x) {
+			System.out.println("hop");
+		}
+	}
+}
