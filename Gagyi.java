23094c522d98608eab93d91be77eb6d2c17c1fe2
kzoltan99 <kzotya99@gmail.com>
2019-09-24 13:06:52 +0200

    Add files via upload

---
+++ Gagyi.java
@@ -0,0 +1,12 @@
+public class Gagyi {
+
+	public static void main(String[] args) {
+	
+		Integer x = new Integer 5;
+		Integer t = new Integer 0;
+
+		while (x <= t && x >= t && t!=x) {
+			System.out.println("hop");
+		}
+	}
+}
